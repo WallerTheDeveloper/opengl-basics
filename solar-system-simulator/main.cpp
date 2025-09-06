9aff4f8dc9cf022f8625617f967d3dde92aedae0
Waller <golo7ov.danil@gmail.com>
2025-09-06 17:01:21 +0200

    Applied texture to rectangle and created shader class

--- solar-system-simulator/main.cpp
+++ solar-system-simulator/main.cpp
@@ -1,40 +1,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
+#include "Shader.h"
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+void processInput(GLFWwindow* window, bool& downWasPressedThisFrame, bool& upWasPressedThisFrame);
 
-void generateBuffers(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO, unsigned int& VAO1, unsigned int& VBO1, unsigned int& EBO1);
+void generateBuffers(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO);
 
-void cleanupVertexArrays(unsigned int& VAO, unsigned int& VAO1);
-void cleanupBuffers(unsigned int& VBO, unsigned int& VBO1, unsigned int& EBO, unsigned int& EBO1);
+void cleanupVertexArrays(unsigned int& VAO);
+void cleanupBuffers(unsigned int& VBO, unsigned int& EBO);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1);\n"
-"}\0";
-const char* fragmentShaderSourceOrange = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
-
-const char* fragmentShaderSourceYellow = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
-"}\n\0";
-
+float lastKeyPressTime = 0.0f;
+const float KEY_COOLDOWN = 0.1f;
 
 int main()
 {
@@ -69,135 +52,41 @@ int main()
         return -1;
     }
 
-
-    // build and compile our shader program
-    // ------------------------------------
-    // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // fragment shader
-    unsigned int fragmentShaderOrange = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShaderOrange, 1, &fragmentShaderSourceOrange, NULL);
-    glCompileShader(fragmentShaderOrange);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShaderOrange, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShaderOrange, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // fragment shader yellow
-    unsigned int fragmentShaderYellow = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShaderYellow, 1, &fragmentShaderSourceYellow, NULL);
-    glCompileShader(fragmentShaderYellow);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShaderYellow, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShaderYellow, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // link shaders
-    unsigned int shaderProgramOrange = glCreateProgram();
-    glAttachShader(shaderProgramOrange, vertexShader);
-    glAttachShader(shaderProgramOrange, fragmentShaderOrange);
-    glAttachShader(shaderProgramOrange, fragmentShaderOrange);
-    glLinkProgram(shaderProgramOrange);
-
-    // link shaders
-    unsigned int shaderProgramYellow = glCreateProgram();
-    glAttachShader(shaderProgramYellow, vertexShader);
-    glAttachShader(shaderProgramYellow, fragmentShaderYellow);
-    glAttachShader(shaderProgramYellow, fragmentShaderYellow);
-    glLinkProgram(shaderProgramYellow);
-
-    // check for linking errors
-    glGetProgramiv(shaderProgramYellow, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgramYellow, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADERYELLOW::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-
-    // check for linking errors
-    glGetProgramiv(shaderProgramOrange, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgramOrange, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADERORANGE::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShaderOrange);
-    glDeleteShader(fragmentShaderYellow);
-
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    float vertices[] = {
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left 
+    float vertices[] = {    
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,   // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // top left 
     };
 
     unsigned int indices[] = {
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+        0, 1, 3, 
+        1, 2, 3
     };
 
-    float triangle[] = {
-        // First Triangle
-        -1.0f, -0.5f, 0.0f, // left bottom
-        0.0f, -0.5f, 0.0f,  // right bottom
-        -0.5f, 0.5f, 0.0f,  // top
-    };
-    float triangle1[] = {
-        // Second Triangle
-        0.5f, 0.5f, 0.0f,   // top
-        1.0f, -0.5f, 0.0f,  // right bottom
-        0.0f, -0.5f, 0.0f,  // left bottom
-    };
-    
-    unsigned int indicesTriangle[] = {
-        0, 1, 2,
-    };
-    unsigned int indicesTriangle1[] = {
-        0, 1, 2
-    };
+    unsigned int VBO, VAO, EBO;
 
-    unsigned int VBO, VAO, EBO, VBO1, VAO1, EBO1;
-
-    generateBuffers(VAO, VBO, EBO, VAO1, VBO1, EBO1);
+    generateBuffers(VAO, VBO, EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesTriangle), indicesTriangle, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    
-    glBindVertexArray(VAO1);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO1);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesTriangle1), indicesTriangle1, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     //glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -209,38 +98,117 @@ int main()
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    Shader shader("triangle.vert", "triangle.frag");
+
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+
+    unsigned char* texture1Data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (texture1Data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture1Data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture 1" << std::endl;
+    }
+    stbi_image_free(texture1Data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    //stbi_set_flip_vertically_on_load(true);
+
+    unsigned char* texture2Data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    if (texture2Data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture2Data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture 1" << std::endl;
+    }
+    stbi_image_free(texture2Data);
+
+    shader.use();
+
+    glUniform1i(glGetUniformLocation(shader.ID, "texture1"), 0); // set it manually
+    shader.setInt("texture2", 1); // or with shader class
+
+    float texturePrioValue = 0.5f;
+
     while (!glfwWindowShouldClose(window))
     {
-        processInput(window);
+        float timeValue = glfwGetTime();
 
+        bool downWasPressedThisFrame = false;  // Initialize to false
+        bool upWasPressedThisFrame = false;    // Initialize to false
+
+
+        processInput(window, downWasPressedThisFrame, upWasPressedThisFrame);
+
+        
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw our first triangle
-        glUseProgram(shaderProgramOrange);
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        shader.setFloat("ourColor", greenValue);
 
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        if (downWasPressedThisFrame && (timeValue - lastKeyPressTime) > KEY_COOLDOWN)
+        {
+            texturePrioValue -= 0.1f;
+            if (texturePrioValue < 0.0f)
+                texturePrioValue = 0.0f;
+            lastKeyPressTime = timeValue;
+        }
+        if (upWasPressedThisFrame && (timeValue - lastKeyPressTime) > KEY_COOLDOWN)
+        {
+            texturePrioValue += 0.1f;
+            if (texturePrioValue > 1.0f)
+                texturePrioValue = 1.0f;
+            lastKeyPressTime = timeValue;
+        }
+        shader.setFloat("texturePriority", texturePrioValue);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glBindVertexArray(0);
 
-        glUseProgram(shaderProgramYellow);
-
-        glBindVertexArray(VAO1);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    cleanupVertexArrays(VAO, VAO1);
-    cleanupBuffers(VBO, VBO1, EBO, EBO1);
+    cleanupVertexArrays(VAO);
+    cleanupBuffers(VBO, EBO);
 
-    glDeleteProgram(shaderProgramOrange);
-    glDeleteProgram(shaderProgramYellow);
+    shader.deleteCurrentProgram();
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -248,38 +216,41 @@ int main()
     return 0;
 }
 
-void cleanupVertexArrays(unsigned int& VAO, unsigned int& VAO1)
+void cleanupVertexArrays(unsigned int& VAO)
 {
     glDeleteVertexArrays(1, &VAO);
-    glDeleteVertexArrays(1, &VAO1);
 }
 
-void cleanupBuffers(unsigned int& VBO, unsigned int& VBO1, unsigned int& EBO, unsigned int& EBO1)
+void cleanupBuffers(unsigned int& VBO, unsigned int& EBO)
 {
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteBuffers(1, &VBO1);
-    glDeleteBuffers(1, &EBO1);
 }
 
-void generateBuffers(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO, unsigned int& VAO1, unsigned int& VBO1, unsigned int& EBO1)
+void generateBuffers(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO)
 {
     glGenVertexArrays(1, &VAO);
-    glGenVertexArrays(1, &VAO1);
 
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
-    
-    glGenBuffers(1, &VBO1);
-    glGenBuffers(1, &EBO1);
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
+void processInput(GLFWwindow* window, bool& downWasPressedThisFrame, bool& upWasPressedThisFrame)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        downWasPressedThisFrame = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        upWasPressedThisFrame = true;
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
